ef5ceea684325d505d6271b9c1c5006de39214c3
jomeiii <stepadres776@gmail.com>
2024-03-24 19:52:57 +0500

    Add Color PIcker

---
+++ ColorTheWholeTown/Assets/CodeBase/ColourPicker/ColourPickerController.cs
@@ -0,0 +1,149 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace CodeBase.ColourPicker
+{
+    public class ColorPickerControl : MonoBehaviour
+    {
+        public float currentHue, currentSat, currentVal;
+
+        [SerializeField] private RawImage _hueImage, _satValImage, _outputImage;
+
+        [SerializeField] private Slider _hueSlider;
+
+        [SerializeField] private TMP_InputField _hexInputField;
+
+        private Texture2D _hueTexture, _svTexture, _outputTexture;
+
+        [SerializeField] private MeshRenderer _changeThisColor;
+
+        [SerializeField] private SvImageControl _svImageControl;
+
+        private void Start()
+        {
+            CreateHueImage();
+            CreateSvImage();
+            CreateOutputImage();
+
+            UpdateOutputColorImage();
+        }
+
+        private void CreateHueImage()
+        {
+            _hueTexture = new Texture2D(1, 16);
+            _hueTexture.wrapMode = TextureWrapMode.Clamp;
+            _hueTexture.name = "HueTexture";
+
+            for (int i = 0; i < _hueTexture.height; i++)
+            {
+                _hueTexture.SetPixel(0, i, Color.HSVToRGB((float)i / _hueTexture.height, 1, 1));
+            }
+
+            _hueTexture.Apply();
+            currentHue = 0;
+
+            _hueImage.texture = _hueTexture;
+        }
+
+        private void CreateSvImage()
+        {
+            _svTexture = new Texture2D(16, 16);
+            _svTexture.wrapMode = TextureWrapMode.Clamp;
+            _svTexture.name = "SVTexture";
+
+            for (int y = 0; y < _svTexture.height; y++)
+            {
+                for (int x = 0; x < _svTexture.width; x++)
+                {
+                    _svTexture.SetPixel(x, y,
+                        Color.HSVToRGB(currentHue, (float)x / _svTexture.width, (float)y / _svTexture.height));
+                }
+            }
+
+            _svTexture.Apply();
+            currentHue = 0;
+            currentVal = 0;
+
+            _satValImage.texture = _svTexture;
+        }
+
+        private void CreateOutputImage()
+        {
+            _outputTexture = new Texture2D(1, 16);
+            _outputTexture.wrapMode = TextureWrapMode.Clamp;
+            _outputTexture.name = "OutputTexture";
+
+            Color currentColor = Color.HSVToRGB(currentHue, 1, 1);
+            for (int i = 0; i < _outputTexture.height; i++)
+            {
+                _outputTexture.SetPixel(0, i, currentColor);
+            }
+
+            _outputTexture.Apply();
+
+            _outputImage.texture = _outputTexture;
+        }
+
+        private void UpdateOutputColorImage()
+        {
+            Color currentColor = Color.HSVToRGB(currentHue, currentSat, currentVal);
+
+            for (int i = 0; i < _outputTexture.height; i++)
+            {
+                _outputTexture.SetPixel(0, i, currentColor);
+            }
+
+            _outputTexture.Apply();
+
+            _hexInputField.text = ColorUtility.ToHtmlStringRGB(currentColor);
+
+            _changeThisColor.material.SetColor("_Color", currentColor);
+        }
+
+        public void SetSv(float s, float v)
+        {
+            currentSat = s;
+            currentVal = v;
+
+            UpdateOutputColorImage();
+        }
+
+        public void UpdateSvImage()
+        {
+            currentHue = _hueSlider.value;
+
+            for (int y = 0; y < _svTexture.height; y++)
+            {
+                for (int x = 0; x < _svTexture.width; x++)
+                {
+                    _svTexture.SetPixel(x, y,
+                        Color.HSVToRGB(currentHue, (float)x / _svTexture.width, (float)y / _svTexture.height));
+                }
+            }
+
+            _svTexture.Apply();
+
+            _satValImage.texture = _svTexture;
+
+            UpdateOutputColorImage();
+        }
+
+        public void OnTextInput()
+        {
+            if (_hexInputField.text.Length < 6)
+                return;
+
+            Color newColor;
+            if (ColorUtility.TryParseHtmlString("#" + _hexInputField.text, out newColor))
+            {
+                Color.RGBToHSV(newColor, out currentHue, out currentSat, out currentVal);
+            }
+
+            _hueSlider.value = currentHue;
+            _hexInputField.text = "";
+
+            UpdateOutputColorImage();
+        }
+    }
+}
---
+++ ColorTheWholeTown/Assets/CodeBase/ColourPicker/SVImageControll.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CodeBase.ColourPicker
+{
+    public class SvImageControl : MonoBehaviour
+    {
+        public event Action pickerPositionChangedEvent;
+        
+        [SerializeField] private Image _pickerImage;
+        private RawImage _SVImage;
+        private ColorPickerControl _colorPickerControl;
+        private RectTransform _rectTransform, _pickerTransform;
+
+        private void Awake()
+        {
+            _SVImage = GetComponent<RawImage>();
+            _colorPickerControl = FindObjectOfType<ColorPickerControl>();
+            _rectTransform = GetComponent<RectTransform>();
+
+            _pickerTransform = _pickerImage.GetComponent<RectTransform>();
+            _pickerTransform.localPosition =
+                new Vector2(-(_rectTransform.sizeDelta.x / 2), -(_rectTransform.sizeDelta.y / 2));
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButton(0) &&
+                RectTransformUtility.RectangleContainsScreenPoint(_rectTransform, Input.mousePosition, Camera.main))
+            {
+                UpdateColor();
+            }
+        }
+
+        private void UpdateColor()
+        {
+            Vector2 pos;
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(_rectTransform,
+                Input.mousePosition,
+                Camera.main, out pos);
+
+            float deltaX = _rectTransform.sizeDelta.x / 2;
+            float deltaY = _rectTransform.sizeDelta.y / 2;
+
+            if (pos.x > deltaX) pos.x = deltaX;
+            else if (pos.x < -deltaY) pos.x = -deltaY;
+            if (pos.y > deltaY) pos.y = deltaY;
+            else if (pos.y < -deltaY) pos.y = -deltaY;
+
+            float x = pos.x + deltaX;
+            float y = pos.y + deltaY;
+
+            float xNorm = x / _rectTransform.sizeDelta.x;
+            float yNorm = y / _rectTransform.sizeDelta.y;
+
+            _pickerTransform.localPosition = pos;
+
+            _colorPickerControl.SetSv(xNorm, yNorm);
+        }
+
+        public void UpdatePickerPos(Vector3 newPos)
+        {
+            _pickerTransform.localPosition = newPos;
+        }
+    }
+}
